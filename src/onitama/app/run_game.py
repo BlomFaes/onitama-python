74c9558ffef2594c9514ad96a84e5ef5618d2981
Bram Faes <bramfaes@icloud.com>
2026-03-09 19:21:22 +0100

    Added some comments

--- src/onitama/app/run_game.py
+++ src/onitama/app/run_game.py
@@ -1,6 +1,7 @@
 from onitama.app.game import initial_state, play_game
 from onitama.app.players.human_player import HumanPlayer
 from onitama.app.players.minimax_player import MinimaxPlayer
+from onitama.app.players.random_player import RandomPlayer
 
 from onitama.domain import cards
 from onitama.domain.models import Player
@@ -24,13 +25,26 @@ def run_game() -> None:
     # Random deal:
     cards_for_game = cards.deal_five(seed=None)
 
+    #Starting state
     state = initial_state(cards_for_game)
+
+    # Player selection:
+
+    # Possibilities:
+    # - Human:
+        # red/blue = HumanPlayer(ui.choose_move)
+    # - Minimax:
+        # red/blue = MinimaxPlayer(player=Player.RED, depth=5)
+    # - Random:
+        # red/blue = RandomPlayer(seed=None)
 
     red = HumanPlayer(ui.choose_move)
     blue = MinimaxPlayer(player=Player.BLUE, depth=5)
 
+    # Run the game
     final_state = play_game(state, red, blue)
     ui.show_state(final_state)
 
+    # Check the winner
     w = winner(final_state)
     print("Winner:", w.value if w else "None (max moves reached)")
